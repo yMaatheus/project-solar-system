5490769849fbea7097e400c7cbf7849153859150
Matheus <stvgamer74@gmail.com>
2022-02-18 15:22:26 -0300

    R02: Criando component SolarSystem

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
   render() {
-    return (<Header />);
+    return (
+      <>
+        <Header />
+        <SolarSystem />
+      </>
+    );
   }
 }
 
