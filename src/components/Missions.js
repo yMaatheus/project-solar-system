b03210126f35f2bd6b8b6d0b989fe0c343b1231f
Matheus <stvgamer74@gmail.com>
2022-02-18 16:15:54 -0300

    R10: Gerando lista de missoes dentro do component Missions usando o component MissionCard como filho

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends React.Component {
   render() {
+    const generateMissions = missions.map(
+      ({ name, year, country, destination }, index) => (
+        <MissionCard
+          key={ index }
+          name={ name }
+          year={ year }
+          country={ country }
+          destination={ destination }
+        />
+      ),
+    );
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        {generateMissions}
       </div>
     );
   }
