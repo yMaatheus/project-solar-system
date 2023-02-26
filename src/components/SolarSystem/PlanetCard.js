66319a44a1a6f4e6a2336fedfebea35122e3a1c0
Matheus <stvgamer74@gmail.com>
2023-02-26 15:45:43 -0300

    feat: :sparkles: Add size prop and PlanetSizes to PlanetCard

--- src/components/SolarSystem/PlanetCard.js
+++ src/components/SolarSystem/PlanetCard.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import * as S from './styles';
+import { PlanetSizes } from '../../data/planets';
 
 class PlanetCard extends React.Component {
   render() {
-    const { planetName, planetImage } = this.props;
+    const { size, planetName, planetImage } = this.props;
+    const PlanetWidth = `${PlanetSizes[size].width}`;
+    const PlanetHeight = `${PlanetSizes[size].height}`;
     return (
       <S.PlanetCard data-testid="planet-card">
         <p data-testid="planet-name">{planetName}</p>
-        <S.PlanetImg src={ planetImage } alt={ `Planeta ${planetName}` } />
+        <S.PlanetImg
+          PlanetWidth={ PlanetWidth }
+          PlanetHeight={ PlanetHeight }
+          src={ planetImage }
+          alt={ `Planeta ${planetName}` }
+        />
       </S.PlanetCard>
     );
   }
 }
 
 PlanetCard.propTypes = {
+  size: PropTypes.number.isRequired,
   planetName: PropTypes.string.isRequired,
   planetImage: PropTypes.string.isRequired,
 };
--- src/components/SolarSystem/SolarSystem.js
+++ src/components/SolarSystem/SolarSystem.js
@@ -3,17 +3,24 @@ import Title from '../Title';
 import Planets from '../../data/planets';
 import PlanetCard from './PlanetCard';
 import * as S from './styles';
+import line from '../../assets/line.svg';
 
 class SolarSystem extends React.Component {
   render() {
     const generatePlanets = Planets.map(
       ({ name, image }, index) => (
-        <PlanetCard key={ index } planetName={ name } planetImage={ image } />
+        <PlanetCard
+          key={ index }
+          size={ index + 1 }
+          planetName={ name }
+          planetImage={ image }
+        />
       ),
     );
     return (
       <S.SolarSystemContainer data-testid="solar-system">
         <Title headline="Planetas" />
+        <S.Line src={ line } alt="Linha" />
         <S.PlanetsContainer>
           {generatePlanets}
         </S.PlanetsContainer>
