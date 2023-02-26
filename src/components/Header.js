1f1589cf411f97f92d679d87248a66518d39b69c
Matheus <stvgamer74@gmail.com>
2023-02-25 21:15:21 -0300

    feat: :sparkles: Update Header component to include logo and styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
+import SolarSystemLogo from '../assets/logo-sistema-solar.svg';
+
+const HeaderStyle = styled.header`
+  display: flex;
+  justify-content: center;
+`;
+
+const Logo = styled.img`
+  max-width: 462px;
+  max-height: 264px;
+  margin: 2.5rem 0;
+`;
 
 class Header extends React.Component {
   render() {
     return (
-      <header>
-        <h1> Sistema Solar </h1>
-      </header>
+      <HeaderStyle>
+        <Logo src={ SolarSystemLogo } alt="Logo Sistema Solar" />
+      </HeaderStyle>
     );
   }
 }
