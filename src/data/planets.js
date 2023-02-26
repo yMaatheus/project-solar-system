78248078dcfc297ebbf96b70d69cd64526cce56c
Matheus <stvgamer74@gmail.com>
2023-02-26 15:45:14 -0300

    feat: :sparkles: Add PlanetSizes object to Planets data

--- src/data/planets.js
+++ src/data/planets.js
@@ -18,4 +18,15 @@ const Planets = [
   { name: 'Netuno', image: Netunoimg },
 ];
 
+export const PlanetSizes = {
+  1: { width: '2.625rem', height: '2rem' },
+  2: { width: '6.375rem', height: '4.75rem' },
+  3: { width: '6.625rem', height: '4.938rem' },
+  4: { width: '4.125rem', height: '3.125rem' },
+  5: { width: '17.625rem', height: '13.125rem' },
+  6: { width: '17rem', height: '12.75rem' },
+  7: { width: '9rem', height: '6.875rem' },
+  8: { width: '9rem', height: '6.75rem' },
+};
+
 export default Planets;
