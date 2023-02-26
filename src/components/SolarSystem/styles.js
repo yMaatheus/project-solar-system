5284823d97857f92df3c75011b0d8cca3d62bd0c
Matheus <stvgamer74@gmail.com>
2023-02-26 15:44:52 -0300

    style: :lipstick: Add style props to PlanetImg and Line components

--- src/components/SolarSystem/styles.js
+++ src/components/SolarSystem/styles.js
@@ -6,14 +6,24 @@ export const SolarSystemContainer = styled.div`
   flex-direction: column;
 `;
 
+export const Line = styled.img`
+  position: relative;
+  top: 128px;
+  z-index: -1;
+`;
+
 export const PlanetsContainer = styled.section`
   display: flex;
   justify-content: center;
+  align-items: center;
 `;
 
 export const PlanetCard = styled.div`
+  text-align: center;
+  margin: 0px 1.5rem;
 `;
 
 export const PlanetImg = styled.img`
-  max-width: 100px;
+  max-width: ${(props) => (props.PlanetWidth ? props.PlanetWidth : '100px')};
+  max-height: ${(props) => (props.PlanetHeight ? props.PlanetHeight : '100px')};
 `;
