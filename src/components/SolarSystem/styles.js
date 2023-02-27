5b633ffe6365a0c21fb8fa62ecafaddba7e23ac9
Matheus <stvgamer74@gmail.com>
2023-02-27 20:35:51 -0300

    style: :lipstick: Update SolarSystem styles for better responsivenes

--- src/components/SolarSystem/styles.js
+++ src/components/SolarSystem/styles.js
@@ -11,6 +11,8 @@ export const Line = styled.img`
   position: relative;
   top: 142px;
   z-index: -1;
+  
+  max-width: 100%;
 `;
 
 export const PlanetsWrapper = styled.section`
@@ -26,6 +28,8 @@ export const Card = styled.div`
   flex-direction: column;
   justify-content: space-between;
   height: 300px;
+
+  overflow-x: hidden;
 `;
 
 export const ImageWrapper = styled.div`
@@ -34,11 +38,18 @@ export const ImageWrapper = styled.div`
   flex-direction: column;
   flex: 1;
   align-items: center;
+
+  overflow-x: hidden;
 `;
 
 export const Image = styled.img`
   max-width: ${(props) => (props.PlanetWidth ? props.PlanetWidth : '100px')};
   max-height: ${(props) => (props.PlanetHeight ? props.PlanetHeight : '100px')};
+
+  @media (max-width: 768px) {
+    max-width: 20px;
+    max-height: 20px;
+  }
 `;
 
 export const PlanetName = styled.p`
