2dcf2206ae94541ebbae56a8c29208531254eab4
Matheus <stvgamer74@gmail.com>
2023-02-27 13:45:45 -0300

    feat: :sparkles: Add styles and icons to MissionCard component

--- src/components/Missions/MissionCard.jsx
+++ src/components/Missions/MissionCard.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import * as S from './styles';
+import dateIcon from '../../assets/date-icon.svg';
+import locationIcon from '../../assets/location-icon.svg';
+import flagIcon from '../../assets/flag-icon.svg';
 
 class MissionCard extends React.Component {
   render() {
     const { name, year, country, destination } = this.props;
     return (
-      <div data-testid="mission-card">
-        <p data-testid="mission-name">{name}</p>
-        <p data-testid="mission-year">{year}</p>
-        <p data-testid="mission-country">{country}</p>
-        <p data-testid="mission-destination">{destination}</p>
-      </div>
+      <S.MissionCard data-testid="mission-card">
+        <S.MissionName data-testid="mission-name">{name}</S.MissionName>
+        <S.AttributesWrapper>
+          <S.MissionAttr data-testid="mission-year">
+            <S.AttrIcon src={ dateIcon } alt="Date Icon" />
+            {year}
+          </S.MissionAttr>
+          <S.MissionAttr padding="1.5" data-testid="mission-country">
+            <S.AttrIcon src={ locationIcon } alt="Location Icon" />
+            {country}
+          </S.MissionAttr>
+          <S.MissionAttr padding="1.5" data-testid="mission-destination">
+            <S.AttrIcon src={ flagIcon } alt="Flag Icon" />
+            {destination}
+          </S.MissionAttr>
+        </S.AttributesWrapper>
+      </S.MissionCard>
     );
   }
 }
